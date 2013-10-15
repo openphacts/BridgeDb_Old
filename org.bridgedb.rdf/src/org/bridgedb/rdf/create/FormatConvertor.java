a5337f6cdc6e749093a280d475ad6f584ab9325f
Christian-B <brenninc@cs.man.ac.uk>
2013-10-15 08:29:10 +0100

    Move class to better package

--- org.bridgedb.rdf/src/org/bridgedb/rdf/create/FormatConvertor.java
+++ org.bridgedb.rdf/src/org/bridgedb/rdf/create/FormatConvertor.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.bridgedb.rdf;
+package org.bridgedb.rdf.create;
 
 import info.aduna.lang.FileFormat;
 import java.io.File;
