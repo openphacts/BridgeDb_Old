076e5127ebd04aebf4c64361de1a865c768833dc
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 12:46:02 +0000

    using class from bio and docs

--- org.bridgedb.rdf/test/org/bridgedb/rdf/BridgeDBRdfHandlerTest.java
+++ org.bridgedb.rdf/test/org/bridgedb/rdf/BridgeDBRdfHandlerTest.java
@@ -1,12 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// BridgeDb,
+// An abstraction layer for identifier mapping services, both local and online.
+//
+// Copyright 2006-2009  BridgeDb developers
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package org.bridgedb.rdf;
 
 import java.io.File;
 import java.util.SortedSet;
 import org.bridgedb.bio.BioDataSource;
+import org.bridgedb.bio.DataSourceTxt;
 import org.bridgedb.utils.Reporter;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -25,7 +41,7 @@ public class BridgeDBRdfHandlerTest {
     @Test
     public void testInit() throws Exception {
         Reporter.println("init");
-        DataSourceTxtReader.init();
+        DataSourceTxt.init();
         UriPattern.registerUriPatterns();
         UriPattern.refreshUriPatterns(); 
         BridgeDBRdfHandler.init();
